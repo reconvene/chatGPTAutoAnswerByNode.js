5ac806d4057cd1f66865cabf90bb8d9965e3de34
reconvene <76833217+reconvene@users.noreply.github.com>
2023-10-01 00:22:41 +0800

    Adapt to the interface of OCS

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //引入所需模块
-const express=require('express');
-const axios=require('axios');
+const express = require('express');
+const axios = require('axios');
 
 // 创建一个express应用
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 const url = 'https://api.chatanywhere.com.cn/v1/chat/completions';
 
 //设置请求参数
-const config={
+const config = {
 	// 设置请求的头部，包含内容类型和授权密钥
 	headers: {
 		'Content-Type': 'application/json',
@@ -23,46 +23,61 @@ const config={
 let data = {
 	"model": "gpt-3.5-turbo",
 	"messages": [{
-		"role": "user", 
+		"role": "user",
 		"content": ""
-	}],  
+	}],
 	"temperature": 0.7
 };
 
-app.get('/api/qa', async (req, res) => {	
+app.get('/api/qa', async (req, res) => {
 	//设置响应头，允许的请求来源
-  res.header('Access-Control-Allow-Origin', '*');
-  // 设置响应头，允许的请求方法
-  res.header('Access-Control-Allow-Methods', 'GET');
-  // 设置响应头，允许的请求头
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  // 获取请求中的question参数，这是客户端发来的问题
-  const query = req.query.question;
-  // 如果没有提供问题，则返回错误信息
-  if (!query) {
-    res.status(400).json({ error: 'No query provided' });
-    return;
-  }
-	
-	//将question参数添加进请求数据中
-	console.log(query);
-	data.messages[0].content=query;
-	
-  //用axios向api获取答案，如果报错则抛出
-	try{
-		let GPTAnswer = await axios.post(url,data,config);
-		console.log(GPTAnswer.data.choices[0]);
-		res.status(200).json({ GPTstatus: 200, title: query, answer: GPTAnswer.data.choices[0].message.content });
+	res.header('Access-Control-Allow-Origin', '*');
+	// 设置响应头，允许的请求方法
+	res.header('Access-Control-Allow-Methods', 'GET');
+	// 设置响应头，允许的请求头
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+	// 获取请求中的question参数，这是客户端发来的问题
+	const query = req.query.question;
+	// 如果没有提供问题，则返回错误信息
+	if (!query) {
+		res.status(400).json({
+			error: 'No query provided'
+		});
 		return;
-	}catch(err){
-			res.status(400).json({ GPTstatus: 400, title: query, answer: undefined});
-			console.log(err);
-			return;
+	};
+
+	//获取问题类型和选项
+	const questionType = req.query.type ? '这道题的类型是:[' + req.query.type + ']\n' : '';
+	const questionOptions = req.query.options ? ',选项为:\n' + req.query.options : '';
+
+	//将完整的question添加进请求数据中
+	console.log(`${questionType}题目是:${query}${questionOptions}`);
+	data.messages[0].content = `${questionType}题目是:${query}${questionOptions}`;
+
+	//用axios向api获取答案，如果报错则抛出
+	try {
+		const GPTAnswer = await axios.post(url, data, config);
+		console.log(GPTAnswer.data.choices[0]);
+		res.status(200).json({
+			GPTstatus: 200,
+			title: query,
+			answer: GPTAnswer.data.choices[0].message.content
+		});
+		return;
+	} catch (err) {
+		res.status(400).json({
+			GPTstatus: 400,
+			title: query,
+			answer: undefined
+		});
+		console.log(err);
+		return;
 	};
 });
+
 // 设置一个监听端口号
 const port = process.env.PORT || 3000;
 // 启动服务器并监听端口号
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
