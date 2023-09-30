f05600b7b9a8f9ec88b9aed1c15333f86d9f598a
reconvene <76833217+reconvene@users.noreply.github.com>
2023-09-30 20:23:29 +0800

    Replace request with axios

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-// 引入express模块
-const express = require('express');
+//引入所需模块
+const express=require('express');
+const axios=require('axios');
 
 // 创建一个express应用
 const app = express();
 
-// 引入request模块
-const request=require('request');
-
 // 设置请求的URL，这是ChatGPT API的地址
 const url = 'https://api.chatanywhere.com.cn/v1/chat/completions';
 
-// 设置请求的头部，包含内容类型和授权密钥
-const headers = {
-	'Content-Type': 'application/json',  
-	'Authorization': 'Bearer yourAPIKey'
+//设置请求参数
+const config={
+	// 设置请求的头部，包含内容类型和授权密钥
+	headers: {
+		'Content-Type': 'application/json',
+		'Authorization': 'Bearer !!!-----yourAPIKey-----!!!'
+	},
+	//设置超时时间
+	timeout: 5000
 };
 
-// 设置请求的数据，包含模型名称，消息列表，随机系数等
+// 设置请求数据，包含模型名称，消息列表，随机系数等
 let data = {
 	"model": "gpt-3.5-turbo",
 	"messages": [{
@@ -26,33 +29,11 @@ let data = {
 	"temperature": 0.7
 };
 
-//获取GPT的回答
-function getGPTAnswer(question){
-	console.log(question);
-	data.messages[0].content=question;
-	return new Promise((reslove,reject) => {
-		let req=request({
-			 url: url,
-			 method: 'POST',
-			 json: true,
-			 headers: headers,
-			 body: data
-		} , (err,gptResponse,body) => {
-			  if(err){
-				  //console.error(err);
-				  reject(err);
-				  req.abort();
-			  };
-			  reslove(body);
-			  req.abort();
-		});
-	});
-};
-
 app.get('/api/qa', async (req, res) => {	
+	//设置响应头，允许的请求来源
   res.header('Access-Control-Allow-Origin', '*');
   // 设置响应头，允许的请求方法
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET');
   // 设置响应头，允许的请求头
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
   // 获取请求中的question参数，这是客户端发来的问题
@@ -62,15 +43,22 @@ app.get('/api/qa', async (req, res) => {
     res.status(400).json({ error: 'No query provided' });
     return;
   }
-  //向api获取答案，如果报错则抛出
-  try{
-	  GPTAnswer= await getGPTAnswer(query);
-	  console.log(GPTAnswer.choices);
-	  res.status(200).json({ GPTstatus: 200, title: query, answer: GPTAnswer.choices[0].message.content });
-  }catch(err){
-	  res.status(400).json({ GPTstatus: 400, title: query, answer: undefined});
-	  console.log(err);
-  };
+	
+	//将question参数添加进请求数据中
+	console.log(query);
+	data.messages[0].content=query;
+	
+  //用axios向api获取答案，如果报错则抛出
+	try{
+		let GPTAnswer = await axios.post(url,data,config);
+		console.log(GPTAnswer.data.choices[0]);
+		res.status(200).json({ GPTstatus: 200, title: query, answer: GPTAnswer.data.choices[0].message.content });
+		return;
+	}catch(err){
+			res.status(400).json({ GPTstatus: 400, title: query, answer: undefined});
+			console.log(err);
+			return;
+	};
 });
 // 设置一个监听端口号
 const port = process.env.PORT || 3000;
